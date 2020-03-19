c8044d8b1a89b2fa24a1e0f457e8a2927c5425a5
DSkirvinC <dskirvinc@gmail.com>
2020-03-19 16:00:29 -0400

    Additions/Amendments made to code as listed in Coding Challenge of Android Fundamentals 09.1: Shared Preferences
    Signed-off-by: DSkirvinC <dskirvinc@gmail.com>

--- app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
+++ app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.hellosharedprefs;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.ContextCompat;
@@ -137,9 +138,10 @@ public class MainActivity extends AppCompatActivity {
                 R.color.default_background);
         mShowCountTextView.setBackgroundColor(mColor);
 
-        // Clear preferences
-        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
-        preferencesEditor.clear();
-        preferencesEditor.apply();
+    }
+
+    public void onSettingsClick(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
